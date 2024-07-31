8a288bcee032ec2cc64b24d6745cf4e652fc6e1d
MaxxLiss <maximlisa@yandex.ru>
2024-07-31 14:23:39 +0300

    hw3 was updated

--- src/main/kotlin/Transformer.kt
+++ src/main/kotlin/Transformer.kt
@@ -1,32 +1,37 @@
-class Transformer {
+object Transformer {
     inline fun <T, C : MutableCollection<T>> removeIf(
         collection: C,
         predicate: (T) -> Boolean
     ) : C {
         val toRemove = mutableListOf<T>()
-        for (el in collection) if (predicate(el)) {
-            toRemove.add(el)
+        collection.forEach {
+            if (predicate(it)) {
+                toRemove.add(it)
+            }
         }
-        for (el in toRemove) {
-            collection.remove(el)
-        }
+        toRemove.forEach { collection.remove(it) }
         return collection
     }
 
-    inline fun <T, C : MutableCollection<T>> map(
+    inline fun <T, reified C : MutableCollection<T>, R> map(
         collection: C,
-        transform: (T) -> T
-    ) : C {
-        val tmp = collection.toList()
-        collection.clear()
-        tmp.forEach { collection.add( transform(it) ) }
-        return collection
+        transform: (T) -> R
+    ) : List<R> {
+        val result = mutableListOf<R>()
+        collection.forEach { result.add( transform(it) ) }
+        return result
     }
 
 }
 
 fun main() {
     val a = mutableListOf(1, 2, 3, 4, 5)
-    println(Transformer().map(a) { it * it })
-    println(Transformer().removeIf(a) { it % 2 == 0 } )
+    println(Transformer.map(a) { it * it })
+    println(Transformer.map(a) { it.toString() })
+    println(Transformer.removeIf(a) { it % 2 == 0 } )
+
+    val b = mutableSetOf(1.0, 2.0, 3.0, 4.0, 5.0)
+    println(Transformer.map(b) { it * it })
+    println(Transformer.map(b) { it.toString() })
+    println(Transformer.removeIf(b) { it - 3 > 0 } )
 }
